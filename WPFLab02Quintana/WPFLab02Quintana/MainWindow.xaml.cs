c8ec55b0782d35b5b648d8765e1af8689d77e761
Maigchan <85888327+Maigchan@users.noreply.github.com>
2023-09-08 23:04:27 -0500

    Add files via upload

---
+++ WPFLab02Quintana/WPFLab02Quintana/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFLab02Quintana
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcceder_Click(object sender, RoutedEventArgs e)
+        {
+            // Verificar las credenciales del usuario
+            string usuario = "quintana";
+            string contraseña = "123";
+
+            if (txtUser.Text == usuario && txtPass.Password == contraseña)
+            {
+                // Las credenciales son válidas, procede a abrir la nueva ventana
+                Ventana ventana = new Ventana();
+                ventana.Show();
+                this.Hide(); // Opcionalmente, puedes ocultar la ventana actual
+            }
+            else
+            {
+                // Mostrar un mensaje de error si las credenciales son incorrectas
+                MessageBox.Show("Credenciales incorrectas. Por favor, verifica el usuario y la contraseña.", "Error de inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            // Cerrar la aplicación cuando se hace clic en "Cancelar"
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ WPFLab02Quintana/WPFLab02Quintana/Ventana.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFLab02Quintana
+{
+    public partial class Ventana : Window
+    {
+        private List<Usuario> usuarios; // Mover la lista al ámbito de la clase
+
+        public Ventana()
+        {
+            InitializeComponent();
+
+            // Crear una lista de datos de prueba
+            usuarios = new List<Usuario>
+            {
+                new Usuario { NombreUsuario = "Usuario1", Planta = "Planta 1" },
+                new Usuario { NombreUsuario = "Usuario2", Planta = "Planta 2" },
+                new Usuario { NombreUsuario = "Usuario3", Planta = "Planta 1" },
+                new Usuario { NombreUsuario = "Usuario4", Planta = "Planta 3" },
+            };
+
+            // Asignar la lista al DataGrid como fuente de datos
+            dataGrid.ItemsSource = usuarios;
+        }
+
+        private void btnFiltrar_Click(object sender, RoutedEventArgs e)
+        {
+            // Obtén los valores seleccionados en el ComboBox y el campo de entrada de Usuario
+            string plantaSeleccionada = (cmbPlanta.SelectedItem as ComboBoxItem)?.Content?.ToString();
+            string usuarioFiltrado = txtUsuario.Text;
+
+
+            // Aplica el filtro a la lista de usuarios
+            List<Usuario> usuariosFiltrados = usuarios;
+
+            if (!string.IsNullOrEmpty(plantaSeleccionada))
+            {
+                usuariosFiltrados = usuariosFiltrados.Where(u => u.Planta == plantaSeleccionada).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(usuarioFiltrado))
+            {
+                usuariosFiltrados = usuariosFiltrados.Where(u => u.NombreUsuario.Contains(usuarioFiltrado)).ToList();
+            }
+
+            // Actualiza la fuente de datos del DataGrid con los usuarios filtrados
+            dataGrid.ItemsSource = usuariosFiltrados;
+        }
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            // Limpiar el ComboBox y el TextBox
+            cmbPlanta.SelectedItem = null;
+            txtUsuario.Text = string.Empty;
+
+            // Restaurar la lista completa de usuarios en el DataGrid
+            dataGrid.ItemsSource = usuarios;
+        }
+
+    }
+
+    public class Usuario
+    {
+        public string NombreUsuario { get; set; }
+        public string Planta { get; set; }
+    }
+}
